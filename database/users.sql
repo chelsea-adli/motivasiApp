8ab90a87efecb979eb3669a446bf8678d4eafe5d
chelsea-adli <muhhammadadli242@gmail.com>
2026-09-11 08:48:56 +0700

    Create users.sql

---
+++ database/users.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE motivasiApp;
+
+USE motivasiApp;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(100) NOT NULL,
+    password VARCHAR(100) NOT NULL
+);
